eb16c3580a304adae8c2c8a77169ff6dc1a2c6bf
DonatoRM <donatoramos@hotmail.com>
2023-08-03 19:49:19 +0200

    Users pagination

--- src/lib/hooks/useFilters.js
+++ src/lib/hooks/useFilters.js
@@ -5,26 +5,35 @@ export const useFilters = () => {
 	const [filters, setFilters] = useState({
 		search: '',
 		onlyActive: false,
-		sortBy: SORT_OPTIONS.DEFAULT
+		sortBy: SORT_OPTIONS.DEFAULT,
+		page: 1,
+		itemsPerPage: 2
 	});
 
 	const setSearch = search => {
-		setFilters({ ...filters, search });
+		setFilters({ ...filters, page: 1, search });
 	};
 	const setOnlyActive = onlyActive => {
 		const newSortBy =
 			onlyActive && filters.sortBy === SORT_OPTIONS.ACTIVE
 				? SORT_OPTIONS.DEFAULT
 				: filters.sortBy;
-		setFilters({ ...filters, sortBy: newSortBy, onlyActive });
+		setFilters({ ...filters, sortBy: newSortBy, page: 1, onlyActive });
 	};
 	const setSortBy = sortBy => {
 		setFilters({ ...filters, sortBy });
 	};
+
+	const setPage = newPage => setFilters({ ...filters, page: newPage });
+	const setItemsPerPage = newItemsPerPage =>
+		setFilters({ ...filters, itemsPerPage: newItemsPerPage });
+
 	return {
-		...filters,
+		filters,
 		setSearch,
 		setOnlyActive,
-		setSortBy
+		setSortBy,
+		setPage,
+		setItemsPerPage
 	};
 };
